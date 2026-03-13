229025fea5207c6bd5050ea52b6a36d3cf677b33
Ricardo Varjão <rvarjao@gmail.com>
2026-03-13 11:22:05 -0300

    Ajustes aula 02

--- aula02/app.py
+++ aula02/app.py
@@ -1,15 +1,18 @@
 import sqlite3
 
+# se conecta com o banco de dados escola.db, ou cria o arquivo se ele não existir
 conn = sqlite3.connect("escola.db")
 
-conn.execute("""
-CREATE TABLE IF NOT EXISTS alunos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT
-)
-""")
+# ler o arquivo ../schema.sql e executar as instruções SQL contidas nele
+# é o mesmo que fizemos manualmente quando colocamos o conteúdo
+# do arquivo no dbFiddle, mas agora automatizado no código Python
+# isso é útil para criar as tabelas e a estrutura do banco de dados de forma programática
+# deve verificar se o banco de dados já existe antes de executar o script, para evitar erros de tabela já existente
+dbExiste = conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='alunos'").fetchone() is not None
+if not dbExiste:
+    schemaDb = open("../schema.sql").read()
+    conn.executescript(schemaDb)
 
-conn.commit()
 
 while True:
     print("\n=== Sistema de Cadastro de Alunos ===")
@@ -20,13 +23,18 @@ while True:
     opcao = input("Escolha uma opção: ")
 
     if opcao == "1":
-        nome = input("Digite o nome do aluno: ")
-        conn.execute("INSERT INTO alunos (nome) VALUES (?)",(nome,))
-        conn.commit()
-        print("Aluno salvo no banco!")
+        nome = input("Nome: ")
+        email = input("Email: ")
+        data_nascimento = input("Data de nascimento: ")
+        # inserir um novo registro na tabela alunos usando SQL
+        # os valores são passados como parâmetros para evitar problemas de segurança (SQL injection)
+        conn.execute("INSERT INTO alunos (nome, email, data_nascimento) VALUES (?, ?, ?)", (nome, email, data_nascimento))
+        conn.commit()  # salvar as alterações no banco de dados
+        print("Aluno cadastrado com sucesso!")
+
 
     elif opcao == "2":
-        cursor = conn.execute("SELECT * FROM alunos")
+        cursor = conn.execute("SELECT id, nome, email, data_nascimento FROM alunos")
         print("\nLista de alunos:")
         for aluno in cursor:
             print(aluno)
